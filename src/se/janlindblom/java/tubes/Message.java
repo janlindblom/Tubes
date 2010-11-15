9a78e43146c24647bc4c41cd13a9f30160c86b63
Jan Lindblom <lindblom.jan@gmail.com>
2010-11-15 15:07:25 +0100

    More stuff!
    Signed-off-by: Jan Lindblom <lindblom.jan@gmail.com>

--- src/se/janlindblom/java/tubes/Message.java
+++ src/se/janlindblom/java/tubes/Message.java
@@ -3,9 +3,50 @@ package se.janlindblom.java.tubes;
 /**
  *
  * @author Jan Lindblom (lindblom.jan@gmail.com)
- * @version 0.1
+ * @version 0.2
  */
-public class Message {
-    private int type;
+public abstract class Message {
+    private int sender;
+    private int recipient;
+    private Type type;
+    private Payload payload;
 
+    public Message() {
+        this.setSender(0);
+        this.setRecipient(0);
+        this.setType(null);
+        this.setPayload(null);
+    }
+
+    public int getSender() {
+        return this.sender;
+    }
+
+    public void setSender(int sender) {
+        this.sender = sender;
+    }
+
+    public int getRecipient() {
+        return this.recipient;
+    }
+
+    public void setRecipient(int recipient) {
+        this.recipient = recipient;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Payload getPayload() {
+        return this.payload;
+    }
+
+    public void setPayload(Payload payload) {
+        this.payload = payload;
+    }
 }
---
+++ src/se/janlindblom/java/tubes/Type.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se.janlindblom.java.tubes;
+
+/**
+ *
+ * @author Jan Lindblom (lindblom.jan@gmail.com)
+ * @version 0.1
+ */
+public class Type {
+
+}
